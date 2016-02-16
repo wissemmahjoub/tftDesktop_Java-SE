aa1593ba2c4421d509f23b21b3d858ca9b61540f
ASUS <ASUS@172.16.204.117>
2016-02-16 20:01:39 +0100

    chamss

--- src/esprit/gui/TFTPidev.java
+++ src/esprit/gui/TFTPidev.java
@@ -20,7 +20,7 @@ public class TFTPidev extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("ChoixEspace.fxml"));
-        
+        //chams gitlab w doctour ya doctour;
         Scene scene = new Scene(root);
         System.out.println("bbbbb");
         stage.setScene(scene);
